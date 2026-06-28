b5ae6a9a2c42499f34cbe7da61c7e2734b85e8f1
Mahmoud_Anwar_wassel <118045444+Mahmoudwassel@users.noreply.github.com>
2026-06-28 19:07:19 +0300

    Add files via upload

--- scritp.js
+++ scritp.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.fa-bars');
 const colse = document.querySelector('.fa-x');
 const nav_ver = document.querySelector('.nav-ver')
+const con_us = document.querySelector('.con-us');
+const moving = document.querySelector('.moving')
+const book =document.querySelector('.book');
+
+const book_item =document.querySelector('.book-item');
+const contac_item =document.querySelector('.contact-item')
 
 menu.addEventListener('click',()=>{
     nav_ver.classList.add('active')
@@ -18,3 +24,32 @@ window.addEventListener('resize',()=>{
         nav_ver.classList.remove('active')
     }
 });
+
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("preloader").style.display = "none";
+    }, 2000); // 2000 ملي ثانية = 2 ثانية
+});
+
+con_us.addEventListener('click',()=>{
+    moving.style.transform='translateX(-100%)';
+    book_item.classList.add('active')
+    contac_item.classList.remove('active')
+
+})
+book.addEventListener('click', () => {
+    moving.style.transform = 'translateX(0%)';
+    book_item.classList.remove('active')
+    contac_item.classList.add('active')
+
+});
+
+
+
+
+
+
+// window.addEventListener("load", () => {
+//     document.getElementById("preloader").style.display = "none";
+// });
